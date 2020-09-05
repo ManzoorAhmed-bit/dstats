8cbc49c73a9c681cde7dd8decc32eaeec46cadbd
ManzoorAhmed-bit <61222629+ManzoorAhmed-bit@users.noreply.github.com>
2020-09-06 02:45:29 +0500

    Update dstats.R

--- R/dstats.R
+++ R/dstats.R
@@ -5,13 +5,12 @@
 dstats <- function(x){
   # 2 rows and 2 columns
   par(mfrow = c(2,2))
-
   # Histogram
   hist(x, col = rainbow(30))
   # Box Plot
   boxplot(x, col = 'blue')
   # line Chart
-  plot(x, type = 'l', xlab = 'x', ylab = 'y')
+  plot(x, type = 'l', xlab = 'x')
   # Density Graph
   plot(density(x))
   par(mfrow = c(1,1))
