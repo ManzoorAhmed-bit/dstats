d9f4b224258dfdf1b66fc7f352fc97ed3f496848
ManzoorAhmed-bit <61222629+ManzoorAhmed-bit@users.noreply.github.com>
2020-09-06 02:28:12 +0500

    Add files via upload

---
+++ R/dstats.R
@@ -0,0 +1,20 @@
+#' Creates histogram, boxplot, line chart, density graph and descriptive summary
+#' @export
+#' @param x numeric variable
+
+dstats <- function(x){
+  # 2 rows and 2 columns
+  par(mfrow = c(2,2))
+
+  # Histogram
+  hist(x, col = rainbow(30))
+  # Box Plot
+  boxplot(x, col = 'blue')
+  # line Chart
+  plot(x, type = 'l', xlab = 'x', ylab = 'y')
+  # Density Graph
+  density(x)
+  par(mfrow = c(1,1))
+  # Descriptive Summary
+  data.frame(min = min(x), median = median(x), mean = mean(x), max = max(x))
+}
